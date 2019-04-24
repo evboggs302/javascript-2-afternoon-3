8bc341b4af4db5b997fe8431ef3eb22bf6379a81
Evan Boggs Jr <evanboggs@Evans-MacBook-Pro.local>
2019-04-24 15:14:34 -0700

    initial commit. prob 1 done. no errors.

--- practice.js
+++ practice.js
@@ -27,7 +27,9 @@
   Then invoke the callback function, passing in the first element in the array as it's argument.
 */
 
-// Code Here 
+function first(arr, callback){
+  return callback(arr[0]);
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
